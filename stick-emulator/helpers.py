8d87026b6fe69da1d73e3c44f98858d9ce946bd3
Francesco Sheiban <frances.j.shei@gmail.com>
2025-04-01 17:45:03 +0200

    Removed obsolete files now organised in submodules

--- stick-emulator/helpers.py
+++
@@ -1,9 +0,0 @@
-
-def flatten_nested_list(nested_list:list) -> list:
-    flat_list = []
-    for item in nested_list:
-        if isinstance(item, list):
-            flat_list.extend(flatten_nested_list(item))
-        else:
-            flat_list.append(item)
-    return flat_list
