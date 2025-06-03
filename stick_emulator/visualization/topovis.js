e7663f6b767a80abc5b200cc17a87963c1066f85
Iñigo Lara <ignigoliz@gmail.com>
2025-06-03 11:18:38 +0200

    Fix visualization issue: group labels are now centered

--- stick_emulator/visualization/topovis.js
+++ stick_emulator/visualization/topovis.js
@@ -14,14 +14,14 @@ fetch('/graph_data')
         });
 
         if (data.groups && data.groups.length > 0) {
-        data.groups.forEach(group => {
-            // group label is added later on for precise positioning
-            g.setNode(group.id, { label: "", width: 600, height: 300, style: "fill:rgb(240, 240, 240); stroke: #333; stroke-width: 2;" });
-            group.nodes.forEach(nodeId => {
-                g.setParent(nodeId, group.id);
+            data.groups.forEach(group => {
+                // group label is added later on for precise positioning
+                g.setNode(group.id, { label: "", width: 600, height: 300, style: "fill:rgb(240, 240, 240); stroke: #333; stroke-width: 2;" });
+                group.nodes.forEach(nodeId => {
+                    g.setParent(nodeId, group.id);
+                });
             });
-        });
-    }
+        }
 
         var render = new dagreD3.render();
         var svg = d3.select("#graph").append("svg")
@@ -63,15 +63,24 @@ fetch('/graph_data')
             .text(d => data.edges.find(edge => edge.uid === d.name).label);
 
         // Style for group label
-        data.groups.forEach(group => {
-            const groupNode = g.node(group.id);
+        inner.selectAll("g.cluster").each(function(d, i) {
+            const bbox = this.getBBox();  // Need the renderedd box to know actual width and height
+
+            const this_group = data.groups.find(group => group.id === this.__data__)
+            const this_group_node = g.node(this_group.id)
+            
+            const labelX = this_group_node.x;  // for some reason, x and y must be retrieved from the group node
+            const labelY = this_group_node.y - bbox.height / 2 - 10;
+            
+            // Center text above group
             inner.append("text")
-                .attr("x", groupNode.x - groupNode.height / 2)
-                .attr("y", groupNode.y - groupNode.width / 2)
+                .attr("x", labelX)
+                .attr("y", labelY)
                 .attr("text-anchor", "middle")
                 .style("font-size", "20px")
                 .style("font-weight", "bold")
-                .text(group.label);
-        });
+                .text(d => this_group.label);
+            });
+
     })
     .catch(error => console.error('Error fetching graph data:', error));
