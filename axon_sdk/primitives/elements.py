506699e0c41c25809cc8eecc7dbe15db1a02819d
DmitriLyalikov <Dlyalikov01@manhattan.edu>
2025-06-20 18:20:57 -0400

    Update license header

--- axon_sdk/primitives/elements.py
+++ axon_sdk/primitives/elements.py
@@ -1,4 +1,5 @@
-# Copyright (C) 2025  Neucom Aps
+# Axon SDK — A simulation framework for spike-timing-based neural computation using the STICK model.
+# Copyright (C) 2024–2025 Neucom ApS
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
