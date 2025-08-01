b2893dd283a66716f67e0ea3d1d2a37807db3886
Iñigo Lara <ignigoliz@gmail.com>
2025-08-01 17:39:29 +0200

    Add timing utility

--- axon_sdk/helpers.py
+++ axon_sdk/helpers.py
@@ -2,10 +2,15 @@ import contextlib
 import time
 
 
-class Timing(contextlib.ContextDecorator):
-    def __enter__(self):
+class Timing(contextlib.ContextDecorator):\
+
+    def __init__(self, its=None):
+        self.its = its
+
+    def __enter__(self, ):
         self.start = time.perf_counter_ns()
 
     def __exit__(self, *exc):
-        self.end = time.perf_counter_ns() - self.start
-        print(f"Execution time: {self.end * 1e-6:6.2f} ms")
+        self.elapsed = time.perf_counter_ns() - self.start
+        print(f"Execution time: {self.elapsed * 1e-6:6.2f} ms")
+        if self.its: print(f"  {(self.elapsed * 1e-6)/self.its:6.2f} ms/it")
