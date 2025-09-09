6e1dcdad0ce1c9751fb2ea31bff8207d6da5529a
Iñigo Lara <ignigoliz@gmail.com>
2025-09-09 10:33:25 +0200

    Cleanup documentation and code comments

--- axon_sdk/__init__.py
+++ axon_sdk/__init__.py
@@ -1,19 +1,3 @@
-"""
-Axon SDK
-========
-
-Axon is a Python-based simulation toolkit for spike-timing-based computation using the STICK (Spike Timing Interval Computational Kernel) model.
-
-It provides:
-- Primitives for defining STICK neurons and synapses.
-- Tools for composing symbolic spiking networks.
-- Compilation routines to map symbolic models to executable primitives.
-- A simulator for event-driven execution and visualization of spike dynamics.
-
-This SDK is part of the Neucom platform developed by Neucom ApS.
-"""
-
-
 from .simulator import Simulator, decode_output, count_spikes
 from .predictive_simulator import PredSimulator
 from .helpers import Timing
