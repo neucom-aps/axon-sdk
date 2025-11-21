3e8bea73b5edb9df7858908ae7cbc4c8170e2bc3
Iñigo Lara <ignigoliz@gmail.com>
2025-11-21 12:29:28 +0100

    Fix failing tests for mul with inputs=0

--- tests/test_mul.py
+++ tests/test_mul.py
@@ -18,6 +18,7 @@ from axon_sdk import Simulator, PredSimulator
         (0.01, 0.99),
         (0.12, 0.33),
         (0.42, 0.42),
+        (0.0, 0.0),
     ],
 )
 def test_mul(val1, val2):
@@ -41,28 +42,6 @@ def test_mul(val1, val2):
         decoded_value, abs=1e-2
     ), f"Expected decoded value {actual_value}, got {decoded_value}"
 
-
-@pytest.mark.parametrize(
-    "val1, val2",
-    [
-        (0.0, 0.0),
-    ],
-)
-def test_mul_zero(val1, val2):
-    encoder = DataEncoder(Tmin=10.0, Tcod=100.0)
-
-    net = MultiplierNetwork(encoder)
-    sim = Simulator(net, encoder)
-    sim.apply_input_value(val1, neuron=net.input1, t0=0)
-    sim.apply_input_value(val2, neuron=net.input2, t0=0)
-    sim.simulate(400)
-
-    spikes = sim.spike_log.get(net.output.uid, [])
-
-    # Network not working when both inputs are 0 since output never reaches threshold
-    assert len(spikes) == 0, f"Expected 0 spikes, got {len(spikes)}"
-
-
 # ------------------ With predictive simulator ------------------
 
 
@@ -80,6 +59,7 @@ def test_mul_zero(val1, val2):
         (0.01, 0.99),
         (0.12, 0.33),
         (0.42, 0.42),
+        (0.0, 0.0),
     ],
 )
 def test_pred_mul(val1, val2):
@@ -98,23 +78,3 @@ def test_pred_mul(val1, val2):
     decoded_value = encoder.decode_interval(interval)
     actual_value = val1 * val2
     assert actual_value == pytest.approx(decoded_value, abs=1e-2)
-
-
-@pytest.mark.parametrize(
-    "val1, val2",
-    [
-        (0.0, 0.0),
-    ],
-)
-def test_pred_mul_zero(val1, val2):
-    encoder = DataEncoder(Tmin=10.0, Tcod=100.0)
-    net = MultiplierNetwork(encoder)
-    sim = PredSimulator(net, encoder, dt=0.01)
-    sim.apply_input_value(val1, neuron=net.input1, t0=0)
-    sim.apply_input_value(val2, neuron=net.input2, t0=0)
-    sim.simulate()
-
-    spikes = sim.spike_log.get(net.output.uid, [])
-
-    # Network not working when both inputs are 0 since output never reaches threshold
-    assert len(spikes) == 0, f"Expected 0 spikes, got {len(spikes)}"
